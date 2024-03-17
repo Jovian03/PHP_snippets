13eee56c6bfa82c653152b6933902d79abd8ae89
Anjima Jinu <143925130+Jovian03@users.noreply.github.com>
2024-03-17 12:35:51 +0530

    Add files via upload

---
+++ openfile.php
@@ -0,0 +1,14 @@
+<?php
+//open a file
+    if (!(file_exists("welcome.txt")))
+    {
+        echo "could not open the file";
+    }
+    
+    else
+    {
+        $text = fopen("welcome.txt","r");
+        var_dump($text);
+    }
+    
+?>
---
+++ readfile.php
@@ -0,0 +1,11 @@
+<?php
+    //read a file
+    $file = fopen("welcome.txt", "r") or exit("Unable to open file!");
+    //Output a line of the file until the end is reached
+    while(!feof($file))
+    {
+        echo fgets($file). "<br>";
+    }
+    fclose($file);
+    
+?>
